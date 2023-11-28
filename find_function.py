478d8fcaebb5ae12cd3a11399beb938d17c498f3
omer-yakici <116022575+omer-yakici@users.noreply.github.com>
2023-11-28 14:24:26 +0300

    Add files via upload

---
+++ find_function.py
@@ -0,0 +1,108 @@
+import random
+import math
+import time
+
+def generate_random_term():
+    if random.random() < 0.5:
+        return f"{random.uniform(-10, 10)}"
+    else:
+        return f"{random.choice(['x', 'y'])}"
+
+def generate_individual():
+    return f"{generate_random_term()} {random.choice(['+', '-', '*', '**', 'math.sqrt'])} {generate_random_term()} + {random.uniform(-10, 10)}"
+
+
+def evaluate_function(expr, x, y):
+    try:
+        result = eval(expr)
+        return result
+    except:
+        # Handle division by zero or other errors
+        return float('inf')
+
+def fitness(individual, points):
+    error = 0
+    for x, y, expected_result in points:
+        result = evaluate_function(individual, x, y)
+        error += abs(result - expected_result)
+    return error
+
+def distance_to_target(individual, points):
+    distances = []
+    for x, y, expected_result in points:
+        result = evaluate_function(individual, x, y)
+        distance = abs(result - expected_result)
+        distances.append(distance)
+    return sum(distances) / len(distances)  # Take mean to return a single value
+
+
+def crossover(parent1, parent2):
+   
+    split_point = random.randint(0, min(len(parent1), len(parent2)) - 1)
+    child = parent1[:split_point] + parent2[split_point:]
+    return child
+
+def mutate(individual, mutation_rate=0.1):
+    mutated_individual = list(individual)
+    for i in range(len(mutated_individual)):
+        if random.random() < mutation_rate:
+            mutation_point = random.randint(0, len(mutated_individual) - 1)
+            mutated_individual[mutation_point] = generate_individual()
+    return ''.join(mutated_individual)
+
+def evolutionary_algorithm(points, population_size=100, generations=1000, crossover_rate=0.8, mutation_rate=0.1, max_stale_generations=3):
+    population = [generate_individual() for _ in range(population_size)]
+    best_distance = float('inf')
+    stale_generations = 0
+
+    for generation in range(generations):
+        population = sorted(population, key=lambda x: fitness(x, points))
+        best_individual = population[0]
+
+        distance = distance_to_target(best_individual, points)
+        print(f"Generation {generation}: Best Function ={best_individual}, Distance = {distance}")
+        time.sleep(0.25)
+
+        
+        if distance == 0:
+            break
+
+        new_population = [best_individual]
+
+        for _ in range(population_size - 1):
+            parent1 = random.choice(population[:50])
+            parent2 = random.choice(population[:50])
+
+            child = crossover(parent1, parent2)
+            child = mutate(child, mutation_rate)
+
+            new_population.extend([child])
+
+        population = new_population
+
+        # Reducing existing diversity so that new individuals emerge and diversity increases further
+        if generation % 80 == 0:
+            population = population[:int(population_size * 0.2)]  # En iyi %80'ini koru
+
+        # If distance does not change, increase stale_generations
+        if distance == best_distance:
+            stale_generations += 1
+        else:
+            stale_generations = 0
+
+        # If there are a certain number of consecutive stales (max_stale_generations) or the distance is 0, change the function
+        if stale_generations >= max_stale_generations or distance == 0:
+            population[1:] = [generate_individual() for _ in range(population_size - 1)]
+            stale_generations = 0
+
+        best_distance = distance
+
+    print("\nBest Function Found:", best_individual)
+    print("Distance:", distance)
+
+# Given points
+points = [(3, 4, 5), (5, 12, 13), (7, 24, 25)]
+
+# Finding the best fitting function using evolutionary algorithm
+
+evolutionary_algorithm(points)
---
+++ find_text.py
@@ -0,0 +1,44 @@
+import random
+import string
+import time
+
+def generate_random_text(length):
+    return ''.join(random.choice(string.ascii_letters + string.digits + ' ') for _ in range(length))
+
+def calculate_fitness(target, text):
+    return sum(1 for a, b in zip(target, text) if a == b)
+
+def mutate(text, target):
+    index = random.randint(0, len(text) - 1)
+    mutated = list(text)
+    mutated[index] = random.choice(string.ascii_letters + string.digits + ' ')
+    mutated = ''.join(mutated)
+
+    if calculate_fitness(target, mutated) > calculate_fitness(target, text):
+        return mutated
+    else:
+        return text
+
+def evolutionary_algorithm(target, population_size=100, generations=1000):
+    population = [generate_random_text(len(target)) for _ in range(population_size)]
+
+    for generation in range(generations):
+        population.sort(key=lambda x: calculate_fitness(target, x), reverse=True)
+
+        best_fit = population[0]
+        print(f"Generation {generation}: {best_fit} (Fitness: {calculate_fitness(target, best_fit)})")
+        time.sleep(0.2)
+
+        if best_fit == target:
+            print("Target achieved!",str(generation),". is the last generation")
+            break
+
+        new_population = [mutate(best_fit, target) for _ in range(population_size - 1)]
+        new_population.append(best_fit)
+        population = new_population
+
+# Your target text
+target_text = "Merhaba Dunya umarim bugun iyisindir"
+
+# Run evolutionary algorithm
+evolutionary_algorithm(target_text)
